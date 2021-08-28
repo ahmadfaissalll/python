e412350a1f92250ced4933e9428f3caa055cc011
Ahmad Faisal <81705914+ahmadfaissalll@users.noreply.github.com>
2021-08-28 21:52:20 +0700

    Update password_generator.py
    has gone through some improvements

--- password_generator.py
+++ password_generator.py
@@ -6,20 +6,37 @@ import random
 import os
 
 # WORDLIST
-names = "q w e r t y u i o p a s d f g h j k l m z x c v b n 1 2 3 4 5 6 7 8 9 0 - _ ~ `! @ # $ % ^ & * ( )"
-# WORDLIST 2 (UPPERCASE LETTER)
-names2 = "q w e r t y u i o p a s d f g h j k l m z x c v b n".upper()
+names = """q w e r t y u i o p a s d f g h j k l m z x c v b n Q W E R T Y U I O P A S D F G H J K L M Z X C V B N
+         1 2 3 4 5 6 7 8 9 0 - _ ~ `! @ # $ % ^ & * ( )"""
 # UNTUK MENGUBAH STRING DIATAS MENJADI LIST
 names = names.split()
-names2 = names2.split()
-# MENG-COMBINE LIST 'names2' KE LIST 'names'
-names.extend(names2)
 
 # STORE HASIL PASSWORD DISINI
 result = str()
 
 # LOOP COUNTER
 a = 1
+
+
+# GENERATOR FUNCTION
+
+def generator():
+    # FOR LOOPS UNTUK MEN-GENERATE PASSWORD DAN MENYIMPAN HASILNYA DI VARIABLE 'result'
+    for x in range(1, 9):
+        # AGAR SESUAI DENGAN JUMLAH ITEM DI LIST 'names' MENURUT INDEX
+        index = len(names) - 1
+        # ME-RANDOM ANGKA SEBANYAK INDEX ITEM DI LIST 'names'
+        _random = random.randint(1, index)
+        # MENGHAPUS ITEM YANG TELAH DI-RANDOM DAN ME-RETURNNYA (TUJUAN CODE INI ADALAH AGAR ITEM
+        # YANG TELAH DI RANDOM BISA DITAMBAHKAN KE VARIABLE 'result' (VARIABLE UNTUK MENYIMPAN HASIL PASSWORD))
+        y = names.pop(_random)
+        # MEMASUKKAN KEMBALI ITEM YANG TELAH DIHAPUS DI ATAS KE LIST (AGAR ITEM TIDAK BERKURANG)
+        names.insert(_random, y)
+        # UNTUK MEMBUAT PERUBAHAN KE GLOBAL VARIABLE
+        global result
+        # MENAMBAHKAN NILAI VARIABLE 'y' KE VARIABLE 'result'
+        result += y
+
 
 # TITLE
 print("\n\tPASSWORD GENERATOR\n")
@@ -43,44 +60,38 @@ while option_input != "terminal" or option_input != "notepad":
 # PROGRAM UNTUK PRINT HASIL DI TERMINAL
 
 # JIKA INPUT SAMA DENGAN 'terminal' KETIKA HURUF INPUT DIRUBAH KE HURUF KECIL DENGAN METHOD '.lower()' MAKA PROGRAM INI AKAN DIEKSEKUSI
-if option_input.lower() == "terminal":
+while option_input.lower() == "terminal":
     # COBA EKSEKUSI INI TAPI JIKA TERJADI ERROR MAKA 'except' YANG AKAN MENANGANINYA
     try:
         # HOW MUCH DO YOU WANNA GENERATE (THIS IS USER INPUT)
-        numb_input = int(input("\n\tHow much do you wanna generate: "))
+        numb_input = input("\n\tHow much do you wanna generate:(type 'quit' to exit) ")
+        # UNTUK KELUAR DARI PROGRAM
+        if numb_input.lower() == "quit":
+            break
         # INI AKAN DIEKSEKUSI SAMPAI LOOP COUNTER ('a') SAMA DENGAN 'numb_input'
-        while a <= numb_input:
+        while a <= int(numb_input):
             # UNTUK MENULIS BARIS KOSONG (UNTUK JARAK) PADA LOOP PERTAMA (HANYA DIEKSEKUSI SEKALI)
             if a == 1:
                 print("")
-            # FOR LOOPS UNTUK MEN-GENERATE PASSWORD DAN MENYIMPAN HASILNYA DI VARIABLE 'result'
-            for x in range(1, 9):
-                # AGAR SESUAI DENGAN JUMLAH ITEM DI LIST 'names' MENURUT INDEX
-                index = len(names) - 1
-                # ME-RANDOM ANGKA SEBANYAK INDEX ITEM DI LIST 'names'
-                _random = random.randint(0, index)
-                # MENGHAPUS ITEM YANG TELAH DI-RANDOM DAN ME-RETURNNYA (TUJUAN CODE INI ADALAH AGAR ITEM
-                # YANG TELAH DI RANDOM BISA DITAMBAHKAN KE VARIABLE 'result' (VARIABLE UNTUK MENYIMPAN HASIL PASSWORD))
-                y = names.pop(_random)
-                # MEMASUKKAN KEMBALI ITEM YANG TELAH DIHAPUS DI ATAS KE LIST (AGAR ITEM TIDAK BERKURANG)
-                names.insert(_random, y)
-                # MENAMBAHKAN NILAI VARIABLE 'y' KE VARIABLE 'result'
-                result += y
+            # CALLING GENERATOR FUNCTION
+            generator()
             # PRINT HASIL
             print(f"  {a}. {result}")
             # MENGOSONGKAN NILAI VARIABLE 'result' UNTUK MENYIMPAN HASIL LOOPS BERIKUTNYA
             result = ""
             # UNTUK MENAMBAHKAN NILAI 1 KE VARIABLE 'a' DI SETIAP LOOPS ( JIKA INI DIHAPUS MAKA PROGRAM TIDAK AKAN BERHENTI (INFINITY LOOPS) )
             a += 1
+        else:
+            a = 1
     # JIKA TERJADI ERROR PADA PROGRAM DI ATAS MAKA INI AKAN DIEKSEKUSI
     except:
-        print("\n  input tidak valid".title())
+        print("\n\tinput tidak valid!!! jangan masukkan selain angka".upper())
 
 
 # PROGRAM UNTUK MENULIS HASIL DI NOTEPAD
 
 # JIKA INPUT SAMA DENGAN 'terminal' KETIKA HURUF INPUT DIRUBAH KE HURUF KECIL DENGAN METHOD '.lower()' MAKA PROGRAM INI AKAN DIEKSEKUSI
-elif option_input.lower() == "notepad":
+if option_input.lower() == "notepad":
     try:
         # HOW MUCH DO YOU WANNA GENERATE (THIS IS USER INPUT)
         numb_input = int(input("\n\tHow much do you wanna generate: "))
@@ -91,19 +102,8 @@ elif option_input.lower() == "notepad":
                 if os.path.exists("password_result.txt"):
                     # JIKA FILE ADA MAKA HAPUS FILE
                     os.remove("password_result.txt")
-            # FOR LOOPS UNTUK MEN-GENERATE PASSWORD DAN MENYIMPAN HASILNYA DI VARIABLE 'result'
-            for x in range(1, 9):
-                # AGAR SESUAI DENGAN JUMLAH ITEM DI LIST 'names' MENURUT INDEX
-                index = len(names) - 1
-                # ME-RANDOM ANGKA SEBANYAK INDEX ITEM DI LIST 'names'
-                _random = random.randint(0, index)
-                # MENGHAPUS ITEM YANG TELAH DI-RANDOM DAN ME-RETURNNYA (TUJUAN CODE INI ADALAH AGAR ITEM
-                # YANG TELAH DI RANDOM BISA DITAMBAHKAN KE VARIABLE 'result' (VARIABLE UNTUK MENYIMPAN HASIL PASSWORD))
-                y = names.pop(_random)
-                # MEMASUKKAN KEMBALI ITEM YANG TELAH DIHAPUS DI ATAS KE LIST (AGAR ITEM TIDAK BERKURANG)
-                names.insert(_random, y)
-                # MENAMBAHKAN NILAI VARIABLE 'y' KE VARIABLE 'result'
-                result += y
+            # CALLING GENERATOR FUNCTION
+            generator()
             # MENULIS KE NOTEPAD
             f = open("password_result.txt", "a")
             f.write(f"{a}. {result}\n")
