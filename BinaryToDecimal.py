0a44b32f33824fe539a019d65bcf1f0696833b79
Ahmad Faisal <81705914+ahmadfaissalll@users.noreply.github.com>
2021-11-07 20:51:48 +0700

    Update BinaryToDecimal.py

--- BinaryToDecimal.py
+++ BinaryToDecimal.py
@@ -1,41 +1,13 @@
-# REGEX (REGULAR EXPRESSION) LIBRARY
-import re
+# Binary to Decimal Converter
 
-# BINER INPUT
-biner_input = input("\nMasukkan angka biner: ")
+def binary_to_decimal(binary):
+    result = 0
+    pangkat = len(binary) - 1
 
-# REGEX UNTUK MENCARI DAN MENGEKSTRAK 0, 1 dan space
-y = re.findall("[0-1- ]", biner_input)
+    for bit in binary:
+        result += int(bit) * pow(2, pangkat)
+        pangkat -= 1
 
-# REGEX UNTUK MENCARI DAN MENGEKSTRAK 0 DAN 1
-x = re.findall("[0-1]", biner_input)
-
-# LIST BINER
-binerList = list(x)
-
-# AGAR URUTANNYA SAMA DENGAN URUTAN SAAT BINER DIINPUT
-binerList.reverse()
-
-# UNTUK PANGKAT
-a = 0
-
-# UNTUK MENYIMPAN HASIL OPERASI
-result = list()
-# UNTUK MENJUMLAHKAN SEMUA ANGKA DI VARIABLE 'result' DI ATAS
-result = sum(result)
-
-# FOR LOOPS (HANYA BERJALAN KETIKA ADA NILAI '1' YANG DIINPUT)
-for x in binerList:
-    # SETIAP DI 'binerList' DIKALIKAN 2 PANGKAT 0 DST
-    operasi = int(x) * pow(2,a)
-    # MENAMBAHKAN HASIL OPERASI KE VARIABLE 'result'
-    result += operasi
-    # MENAMBAHKAN NILAI PANGKAT
-    a += 1
-
-# AGAR URUTAN BINER SESUAI DENGAN SAAT BINER DIINPUT
-binerList.reverse()
-# UNTUKK MERUBAH LIST KE STRING
-y = "".join(y)
-# PRINT HASIL
-print(f"\n{y} in binary = {result} in decimal")
+    return str(result)
+        
+print('\nDecimal:', '.'.join(map(binary_to_decimal, input("Binary (separated by '.'): ").split('.'))))
