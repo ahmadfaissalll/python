054fec2e76f10c3726428ce3a73093f0412a960e
Ahmad Faisal <81705914+ahmadfaissalll@users.noreply.github.com>
2021-11-30 19:38:45 +0700

    Update GuiIpAddressConverter.py

--- GuiIpAddressConverter.py
+++ GuiIpAddressConverter.py
@@ -9,31 +9,36 @@ class IpAddressConverter:
 
     def main_window(self):
         # create root window with title
-        root_window = tk.Tk(className=" Convert IP Address to Binary")
+        root_window = tk.Tk(className=" IP Address to Binary Converter")
 
         # mengubah ukuran window saat pertama kali dibuka
-        root_window.geometry("500x400")
+        root_window.geometry("700x450")
 
         # agar ukuran windownya tidak bisa dirubah (fix size)
         root_window.resizable(False, False)
 
         # to give a caption
-        caption = tk.Label(root_window, text='IP Address (pisahkan dengan titik)').pack()
+        caption = tk.Label(root_window, text='IP Address (separate with . (dot))', font=("Arial", 13)).pack(pady=15)
 
         # input ip address
-        inputIpAddress = tk.Entry(root_window)
-        inputIpAddress.pack()
+        inputIpAddress = tk.Entry(root_window, fg="#000000", bg='lightblue', width='37', font=("Arial", 11))
+        inputIpAddress.pack(pady=3)
 
         # displaying conversion result
         def change_binary_result():
-            binary_result.config(text=f"Binary: {'.'.join(map(self.decimal_to_binary, inputIpAddress.get().split('.')))}")
-            binary_result.pack()
+            try:
+                if (inputIpAddress.get()):
+                    binary_result.config(text=f"Binary: {'.'.join(map(self.decimal_to_binary, inputIpAddress.get().strip('.').split('.')))}")
+            except ValueError:
+                from tkinter import messagebox
+                messagebox.showerror(title="Error", message="Invalid Input")
+
 
         # button
-        myButton = tk.Button(root_window, text='Convert', command=change_binary_result).pack()
+        myButton = tk.Button(root_window, text='Convert', command=change_binary_result, font=("Arial", 13), padx='10' ,pady="1.5").pack(pady=5)
 
         # text initial
-        binary_result = tk.Label(root_window, text="")
+        binary_result = tk.Label(root_window, font=("Arial", 12))
         binary_result.pack()
 
         # start app
@@ -58,5 +63,6 @@ class IpAddressConverter:
         # return binary with correct order
         return binary[::-1]
 
-# start app
-IpAddressConverter(True)
+
+if __name__ == "__main__":
+    IpAddressConverter(True)
